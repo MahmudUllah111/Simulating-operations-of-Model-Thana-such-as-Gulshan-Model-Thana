c2219f6abdd3c0b95cfaaaffc9d0530d36f7de9a
R3tr0-228 <2230328@iub.edu.bd>
2025-03-24 13:43:02 +0600

    Added exports and opens detail in the module part.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,6 +7,6 @@ module iub.gulshanmodelthana {
     exports iub.gulshanmodelthana;
     exports iub.gulshanmodelthana.m1_mahmudullah_2230406;
     opens iub.gulshanmodelthana.m1_mahmudullah_2230406 to javafx.fxml;
-
-
+    exports iub.gulshanmodelthana.m2_aumio_2230328;
+    opens iub.gulshanmodelthana.m2_aumio_2230328 to javafx.fxml;
 }
