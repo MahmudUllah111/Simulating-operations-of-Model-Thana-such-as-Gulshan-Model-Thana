dabd9338179a68e5b7a8f0c58d5c607b88223c81
R3tr0-228 <2230328@iub.edu.bd>
2025-04-04 23:48:43 +0600

    connected fxml

--- src/main/java/iub/gulshanmodelthana/m1_mahmudullah_2230406/OcExtcooRequestController.java
+++ src/main/java/iub/gulshanmodelthana/m1_mahmudullah_2230406/OcExtcooRequestController.java
@@ -1,4 +1,55 @@
 package iub.gulshanmodelthana.m1_mahmudullah_2230406;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
 public class OcExtcooRequestController {
+
+    @FXML
+    private DatePicker dateDatePicker;
+
+    @FXML
+    private ImageView imageUser;
+
+    @FXML
+    private Label labName;
+
+    @FXML
+    private TextArea purposeTextArea;
+
+    @FXML
+    void backMessage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void notClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sandAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void sendRequestButton(ActionEvent event) {
+
+    }
+
 }
