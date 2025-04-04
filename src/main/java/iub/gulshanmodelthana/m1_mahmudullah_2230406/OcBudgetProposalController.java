7b0bd0294baa93f6d818b0969559060d18e69455
MahmudUllah111 <mahmudullahsets@gmail.com>
2025-04-05 00:08:23 +0600

    Added all the fxml and controller for the Officer in Charge user.

---
+++ src/main/java/iub/gulshanmodelthana/m1_mahmudullah_2230406/OcBudgetProposalController.java
@@ -0,0 +1,74 @@
+package iub.gulshanmodelthana.m1_mahmudullah_2230406;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class OcBudgetProposalController {
+
+    @FXML
+    private TableColumn<?, ?> ammountTableColumn;
+
+    @FXML
+    private ImageView imageUser;
+
+    @FXML
+    private TableColumn<?, ?> itemNameTableColumn;
+
+    @FXML
+    private Label labName;
+
+    @FXML
+    private TableColumn<?, ?> purposeTableColumn;
+
+    @FXML
+    private TableColumn<?, ?> submissionDateTableColumn;
+
+    @FXML
+    private TableView<?> submittedBudgetTableView;
+
+    @FXML
+    void approveSelectedBudgetButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void backMessage(MouseEvent event) {
+
+    }
+
+    @FXML
+    void denySelectedBudget(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void mailClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void notClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void reviewInDetailsButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sandAction(MouseEvent event) {
+
+    }
+
+}
