96aa6dd00910597a979e4b0555dee108772ae513
R3tr0-228 <2230328@iub.edu.bd>
2025-04-04 16:11:53 +0600

    si dashboard controller

---
+++ src/main/java/iub/gulshanmodelthana/m2_aumio_2230328/SiDashboardController.java
@@ -0,0 +1,78 @@
+package iub.gulshanmodelthana.m2_aumio_2230328;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class SiDashboardController {
+
+    @FXML
+    private ImageView imageUser;
+
+    @FXML
+    void arrestWarrentOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void bailApplicationOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void caseManagementOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void courtCasesOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void evidenceLogsOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOutOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mailClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void notClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void petrolLogsOnCLick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportsOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void sandAction(MouseEvent event) {
+
+    }
+
+    @FXML
+    void suspectDatabaseOnClick(ActionEvent event) {
+
+    }
+
+}
