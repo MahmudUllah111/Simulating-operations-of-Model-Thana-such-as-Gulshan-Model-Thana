036bb9b3ede3011826b43449f2f0040668183f52
R3tr0-228 <2230328@iub.edu.bd>
2025-04-08 22:37:12 +0600

    Admin dashboard fxml and controller

---
+++ src/main/java/iub/gulshanmodelthana/m2_aumio_2230328/AdminDashboardController.java
@@ -0,0 +1,79 @@
+package iub.gulshanmodelthana.m2_aumio_2230328;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class AdminDashboardController {
+
+    @FXML
+    private ImageView imageInbox;
+
+    @FXML
+    private ImageView imageNotice;
+
+    @FXML
+    private ImageView imageUser;
+
+    @FXML
+    void financeOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inventoryOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void leagalDocumentsOnCLick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void logClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logOutOnCLick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mailClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void maintainanceOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void monthlyReportOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void notClick(MouseEvent event) {
+
+    }
+
+    @FXML
+    void noticesOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void personnelOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void vehicleOnCLick(ActionEvent event) {
+
+    }
+
+}
