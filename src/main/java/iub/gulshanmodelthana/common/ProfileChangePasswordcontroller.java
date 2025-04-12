3c12cece8ff11e78eb12fd594806aabb35a65294
R3tr0-228 <2230328@iub.edu.bd>
2025-04-12 11:10:59 +0600

    common profile

---
+++ src/main/java/iub/gulshanmodelthana/common/ProfileChangePasswordcontroller.java
@@ -0,0 +1,18 @@
+package iub.gulshanmodelthana.common;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ProfileChangePasswordcontroller {
+
+    @FXML
+    void backOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void changeCurrentPasswordOnClick(ActionEvent event) {
+
+    }
+
+}
