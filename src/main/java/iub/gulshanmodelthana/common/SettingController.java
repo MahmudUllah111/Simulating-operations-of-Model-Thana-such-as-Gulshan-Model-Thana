a53e98d52553980e85942ff903f75d791896a7dd
MahmudUllah111 <mahmudullahsets@gmail.com>
2025-04-12 13:39:24 +0600

    added setting controller codes

--- src/main/java/iub/gulshanmodelthana/common/SettingController.java
+++ src/main/java/iub/gulshanmodelthana/common/SettingController.java
@@ -5,14 +5,13 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.shape.Circle;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -45,6 +44,8 @@ public class SettingController {
 
     @FXML
     private AnchorPane paneSide;
+    @FXML
+    private Label labWarning;
 
     @FXML
     void logClick(MouseEvent event) throws IOException {
@@ -97,6 +98,32 @@ public class SettingController {
 
     @FXML
     void saveClick(MouseEvent event) {
+        String currentPassword = enCurrentPassword.getText();
+        String newPassword = enNewPassword.getText();
+        String confirmPassword = enConfirmPassword.getText();
+
+        if (currentPassword.isEmpty() || newPassword.isEmpty() || confirmPassword.isEmpty()) {
+            labWarning.setText(" Please fill all the fields.");
+            labWarning.setTextFill(Color.RED);
+            labWarning.setStyle("-fx-border-color: red");
+            labWarning.setVisible(true);
+            return;
+        }
+
+        if (!newPassword.equals(confirmPassword)) {
+            labWarning.setText(" Passwords do not match.");
+            labWarning.setTextFill(Color.RED);
+            labWarning.setStyle("-fx-border-color: red");
+            labWarning.setVisible(true);
+            return;
+        }
+
+        System.out.println("Previous Password: " + currentPassword);
+        System.out.println("New Password: " + newPassword);
+        labWarning.setText(" Password updated successfully.");
+        labWarning.setTextFill(Color.GREEN);
+        labWarning.setStyle("-fx-border-color: green");
+        labWarning.setVisible(true);
 
     }
     @FXML
