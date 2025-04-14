6e816b65f99cd57ea206d2e1e829b0d20d42a411
R3tr0-228 <2230328@iub.edu.bd>
2025-04-14 23:55:36 +0600

    si controller code(1/8)

--- src/main/java/iub/gulshanmodelthana/m2_aumio_2230328/Si_OC_ReportsController.java
+++ src/main/java/iub/gulshanmodelthana/m2_aumio_2230328/Si_OC_ReportsController.java
@@ -1,18 +1,22 @@
 package iub.gulshanmodelthana.m2_aumio_2230328;
 
 import iub.gulshanmodelthana.HelloApplication;
+import iub.gulshanmodelthana.m1_mahmudullah_2230406.OcCrimeReport_Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
+
+import static iub.gulshanmodelthana.m1_mahmudullah_2230406.OcCrimeReportController.messageList;
 
 public class Si_OC_ReportsController {
+
     @FXML
     private TextArea crimeSceneDetailsTextArea;
 
@@ -28,56 +32,48 @@ public class Si_OC_ReportsController {
     @FXML
     private ComboBox<String> reportTypeComboBox;
 
-    @Deprecated
-    void addEvidenceOnClick(ActionEvent event) {
-
-    }
-
     @FXML
     void backOnClick(ActionEvent event) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("m2_aumio_2230328/si_dashboard.fxml"));
         Scene nextScene = new Scene(fxmlLoader.load());
 
         Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle("dashboard");
+        stage.setTitle("SI Dashboard");
         stage.setScene(nextScene);
         stage.show();
-
-    }
-
-    @Deprecated
-    void fileclick(MouseEvent event) {
-
     }
 
     @FXML
-    void sendReportOnClick(ActionEvent event) {
-        String report_name = reportNameTextField.getText();
+    void sendReportOnClick(ActionEvent event) throws IOException {
+        String reportName = reportNameTextField.getText();
         String location = locationTextField.getText();
-        String details = crimeSceneDetailsTextArea.getText();
-        String date = datePicker.getId();
-        String report_type = reportTypeComboBox.getTypeSelector();
+        LocalDate date = datePicker.getValue();
+        String crimeSceneDetails = crimeSceneDetailsTextArea.getText();
+        String reportType = reportTypeComboBox.getValue();
 
-
-        if (report_name.isEmpty() || location.isEmpty() || report_type == null || details.isEmpty()) {
+        if (reportName.isEmpty() || location.isEmpty() || reportType == null || crimeSceneDetails.isEmpty() || date == null) {
             Alert warning = new Alert(Alert.AlertType.ERROR);
             warning.setContentText("Please fill in all fields.");
             warning.show();
             return;
-
-
         }
 
-    }
 
+        OcCrimeReport_Model report = new OcCrimeReport_Model(reportName, location, date, reportType, crimeSceneDetails);
+        messageList.add(report);
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("m1_mahmudullah_2230406/oc_crimereport.fxml"));
+        Scene nextScene = new Scene(fxmlLoader.load());
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("OC Crime Reports");
+        stage.setScene(nextScene);
+        stage.show();
+    }
 
     @FXML
-    void initialize(){
-        reportTypeComboBox.getItems().addAll("Traffic Incidents","Murder","Burglary","Other`type");
+    void initialize() {
+        reportTypeComboBox.getItems().addAll("Traffic Incidents", "Murder", "Burglary", "Theft", "Other");
     }
 }
-
-
-
-
-
