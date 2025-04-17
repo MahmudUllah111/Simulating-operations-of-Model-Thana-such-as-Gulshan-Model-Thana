2728ec8359ee4c5e721032447f97819131a91363
Jannati <jannat51923@gmail.com>
2025-04-18 05:35:50 +0600

    added newGD controller and fxml file

---
+++ src/main/java/iub/gulshanmodelthana/m3_jannati_2330003/newGDController.java
@@ -0,0 +1,34 @@
+package iub.gulshanmodelthana.m3_jannati_2330003;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class newGDController {
+    @javafx.fxml.FXML
+    private Button saveGd;
+    @javafx.fxml.FXML
+    private TextField GDIDField;
+    @javafx.fxml.FXML
+    private TextArea GDDetailsTextArea;
+    @javafx.fxml.FXML
+    private TextField complaintNameField;
+    @javafx.fxml.FXML
+    private TextField ContactNoField;
+    @javafx.fxml.FXML
+    private AnchorPane gdDatepicker;
+
+    @javafx.fxml.FXML
+    public void saveGdButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void backToDashBoardButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewAllGdButton(ActionEvent actionEvent) {
+    }
+}
